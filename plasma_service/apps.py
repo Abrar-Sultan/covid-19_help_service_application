3eba4bac5d762c4c83ebd80cc654de0be8096d2e
Abrar-Sultan <abrarsultan152@gmail.com>
2020-09-22 13:33:20 +0600

    Initial Setup for Project

--- plasma_service/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PlasmaserviceConfig(AppConfig):
-    name = 'plasma_service'
--- plasma_service/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def request_plasma(request):
-    return render(request, 'request_plasma_form.html')

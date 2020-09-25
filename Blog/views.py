6bd57a7d7671b5bf591e39775a3585eac25ef92a
sakibalamin <sakibalamin162@gmail.com>
2020-09-26 02:03:43 +0600

    created Blog url & initial view

--- Blog/views.py
+++ Blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# @desc Blog Page
+# @route http://127.0.0.1:8000/blog
+
+
+def blog(request):
+    return HttpResponse('<h1> This is the Blog App! <h1>')

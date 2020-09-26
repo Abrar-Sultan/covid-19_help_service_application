065a3448494d96fa8a4808d0faf01171f122610a
sakibalamin <sakibalamin162@gmail.com>
2020-09-26 22:26:53 +0600

    template added for view & form added for consult with doctor with bootstrap form

--- consultWithDoctors/views.py
+++ consultWithDoctors/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ConsultWithDoctor
 
 
 def index(request):
-    return HttpResponse('<h1> This is the consultWithDoctor Page </h1>')
+    return render(request, 'index.html')
 

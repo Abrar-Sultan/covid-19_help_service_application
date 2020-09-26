4bed2cba909106e45b6331e9abdc40a72efce889
sakibalamin <sakibalamin162@gmail.com>
2020-09-26 16:21:09 +0600

    DB model added for consultWithDoctors

--- consultWithDoctors/models.py
+++ consultWithDoctors/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class ConsultWithDoctor(models.Model):
+    name: models.CharField(max_length=50)
+    age: models.IntegerField(default=0)
+    nid: models.CharField(max_length=50)
+    blood_group: models.CharField(max_length=50)

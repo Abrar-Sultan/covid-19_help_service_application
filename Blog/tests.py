72d247bb79eee0a8d63e9c59f5147f6650df965e
sakibalamin <sakibalamin162@gmail.com>
2020-09-27 22:15:42 +0600

    revert commit

--- Blog/tests.py
+++ Blog/tests.py
@@ -1,17 +1,17 @@
 import unittest
-from .models import Blog
-
-
-class TestBlog(unittest.TestCase):
-    def test_blog(self):
-        pass
-
-    def test_url(self):
-        pass
-
-    def test_view(self):
-        pass
-
-
-if __name__ == '__main__':
-    unittest.main()
+# from .models import Blog
+#
+#
+# class TestBlog(unittest.TestCase):
+#     def test_blog(self):
+#         pass
+#
+#     def test_url(self):
+#         pass
+#
+#     def test_view(self):
+#         pass
+#
+#
+# if __name__ == '__main__':
+#     unittest.main()

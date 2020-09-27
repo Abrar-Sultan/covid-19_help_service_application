a74dc79a74f09c89dc94ffa90162dbd6ca0450a1
sakibalamin <sakibalamin162@gmail.com>
2020-09-27 15:24:07 +0600

    unittest imported & class created

--- Blog/tests.py
+++ Blog/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+import unittest
+from .models import Blog
 
-# Create your tests here.
+
+class TestBlog(unittest.TestCase):
+    def test_blog(self):
+        pass
+
+    def test_url(self):
+        pass
+
+    def test_view(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()

a8f50d3e64e022ee62be6dd19eb2fa7aba6e74dd
sakibalamin <sakibalamin162@gmail.com>
2020-09-27 15:27:03 +0600

    unittest imported & class created for consultWithDoctor

--- consultWithDoctors/tests.py
+++ consultWithDoctors/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+import unittest
+from .models import ConsultWithDoctor
 
-# Create your tests here.
+
+class TestConsultWithDoctor(unittest.TestCase):
+    def test_consult(self):
+        pass
+
+    def test_url(self):
+        pass
+
+    def test_view(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
